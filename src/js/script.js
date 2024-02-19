455f1f7014ea5c8f906c2a98fadb627023fbb158
junyahaga <n0812034@yahoo.co.jp>
2024-02-19 20:33:14 +0900

    2024/2/19

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,24 @@
+jQuery(function ($) { // この中であればWordPressでも「$」が使用可能になる
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  //Campaign swiper
+  var swiper = new Swiper(".js-features-swiper", {
+    allowTouchMove: false, // マウスでの操作を無効化
+    centeredSlides: true, // 中央にスライドを配置
+    slidesPerView: "auto",
+    //スライド枚数 1.26枚
 
-
+    //〇〇以上の画面幅で〇〇枚のスライドと幅が〇〇pxと設定する
+    breakpoints: {
+      768: {
+        spaceBetween: 40
+      }
+    },
+    spaceBetween: 24,
+    speed: 5000,
+    loop: true,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false
+    },
+  });
 });
